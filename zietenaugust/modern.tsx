9cd8772c64a3dededf63be22fac2fc1d95efd7e4
Robert Köpferl <rob@os23.de>
2020-11-28 22:15:37 +0100

    verschoben

--- zietenaugust/modern.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="modern" tilewidth="32" tileheight="32" tilecount="192" columns="16">
- <image source="modern.png" width="512" height="384"/>
-</tileset>
